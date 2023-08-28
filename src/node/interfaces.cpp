2b53f6cc87f10e0de69995ba53ce42879d9e05c9
Andrew Chow <github@achow101.com>
2023-08-28 16:09:49 -0400

    interfaces: Also retrieve undo data in findBlock

--- src/node/interfaces.cpp
+++ src/node/interfaces.cpp
@@ -49,6 +49,7 @@
 #include <sync.h>
 #include <txmempool.h>
 #include <uint256.h>
+#include <undo.h>
 #include <univalue.h>
 #include <util/check.h>
 #include <util/result.h>
@@ -431,6 +432,10 @@ bool FillBlock(const CBlockIndex* index, const FoundBlock& block, UniqueLock<Rec
     if (block.m_data) {
         REVERSE_LOCK(lock);
         if (!blockman.ReadBlockFromDisk(*block.m_data, *index)) block.m_data->SetNull();
+    }
+    if (block.m_undo) {
+        REVERSE_LOCK(lock);
+        if (!blockman.UndoReadFromDisk(*block.m_undo, *index)) block.m_undo->vtxundo.clear();
     }
     block.found = true;
     return true;

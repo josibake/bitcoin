76a13a040b43e7107200f77b7c326a7b200631eb
josibake <josibake@protonmail.com>
2023-09-14 14:55:35 +0200

    Implement BIP352: Silent Payments
    Implement sending and receiving, per BIP352. This is done without
    requiring a full wallet, in order to simplify unit testing and to create
    a more clear boundary as to what pertains to the BIP and what is left to
    the wallet to decided as an implementation.
    
    To correctly hash the public keys, per the BIP, update the HashWriter.write
    method to accept a Span<unsigned char>. This allows us to easily pass a
    public key in for hashing, where only the data from the public key is hashed.

---
+++ src/wallet/silentpayments.h
@@ -0,0 +1,23 @@
+#ifndef BITCOIN_WALLET_SILENTPAYMENTS_H
+#define BITCOIN_WALLET_SILENTPAYMENTS_H
+
+#include <addresstype.h>
+#include <coins.h>
+#include <key_io.h>
+#include <undo.h>
+
+namespace wallet {
+CPubKey SumInputPubKeys(const std::vector<CPubKey>& input_pubkeys); // For the unit tests
+uint256 ComputeSilentPaymentLabelTweak(const CKey& scan_key, const int m);
+uint256 ComputeInputHash(const std::vector<COutPoint>& tx_outpoints, const CPubKey& sum_input_pubkeys);
+uint256 ComputeSharedSecretTweak(const CPubKey& ecdh_pubkey, const uint32_t output_index);
+V0SilentPaymentDestination GenerateSilentPaymentLabeledAddress(const V0SilentPaymentDestination& receiver, const uint256& label);
+CPubKey ComputeECDHSharedSecret(const CKey& scan_key, const CPubKey& sender_public_key, const uint256& outpoints_hash);
+std::vector<uint256> GetTxOutputTweaks(const CPubKey& spend_pubkey, const CPubKey& ecdh_pubkey, std::vector<XOnlyPubKey> output_pub_keys, const std::map<CPubKey, uint256>& labels);
+std::optional<std::pair<uint256, CPubKey>> GetSilentPaymentTweakDataFromTxInputs(const std::vector<CTxIn>& vin, const std::map<COutPoint, Coin>& coins);
+
+CPubKey CreateOutput(const CKey& ecdh_scalar, const CPubKey& scan_pubkey, const CPubKey& spend_pubkey, const uint32_t output_index);
+std::map<size_t, WitnessV1Taproot> GenerateSilentPaymentTaprootDestinations(const CKey& ecdh_scalar, const std::map<size_t, V0SilentPaymentDestination>& sp_dests);
+CKey PrepareScalarECDHInput(const std::vector<std::pair<CKey, bool>>& sender_secret_keys, const std::vector<COutPoint>& tx_outpoints);
+} // namespace wallet
+#endif // BITCOIN_WALLET_SILENTPAYMENTS_H

7453dbb8d7808deb2aec14252ab85ab27d287674
josibake <josibake@protonmail.com>
2024-01-31 15:40:57 +0100

    index: add start_height to base index
    This allows indexing to start from a specified start_height (default 0).
    This is particularly useful for blockfilter indexes in that a new filter type could
    be defined where the filter is only relevant after a certain block height (e.g.
    a filter for only taproot scriptPubKeys).
    
    Although this is currently not possible, start_height would also be needed if we wanted
    to enable an index in pruned mode (e.g. having a txindex for recent blocks).
    
    Co-Authored-by: Fabian Jahr <fjahr@protonmail.com>

--- src/index/base.h
+++ src/index/base.h
@@ -107,6 +107,7 @@ protected:
     std::unique_ptr<interfaces::Chain> m_chain;
     Chainstate* m_chainstate{nullptr};
     const std::string m_name;
+    const int m_start_height{0};
 
     void BlockConnected(ChainstateRole role, const std::shared_ptr<const CBlock>& block, const CBlockIndex* pindex) override;
 
@@ -132,12 +133,15 @@ protected:
     void SetBestBlockIndex(const CBlockIndex* block);
 
 public:
-    BaseIndex(std::unique_ptr<interfaces::Chain> chain, std::string name);
+    BaseIndex(std::unique_ptr<interfaces::Chain> chain, std::string name, int start_height = 0);
     /// Destructor interrupts sync thread if running and blocks until it exits.
     virtual ~BaseIndex();
 
     /// Get the name of the index for display in logs.
     const std::string& GetName() const LIFETIMEBOUND { return m_name; }
+
+    /// Get the start height this index was created with (0 if from genesis)
+    int GetStartHeight() const LIFETIMEBOUND { return m_start_height; }
 
     /// Blocks the current thread until the index is caught up to the current
     /// state of the block chain. This only blocks if the index has gotten in

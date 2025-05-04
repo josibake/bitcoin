8526fd7812d80c07c02be4d604eaa93283deeb9e
TheCharlatan <seb.kung@gmail.com>
2025-05-04 11:11:38 +0200

    blockstorage: Remove BlockTreeDB dead code
    This is not called by anything anymore, so just remove it.
    
    This commit is part of a series to replace the leveldb database
    currently used for storing block indexes and headers with a flat file
    storage. This is motivated by the kernel library, where the usage of
    leveldb is a limiting factor to its future use cases. It also offers better
    performance and has a smaller on-disk footprint, though this is mostly
    negligible in the grand scheme of things.

--- src/node/blockstorage.cpp
+++ src/node/blockstorage.cpp
@@ -56,15 +56,6 @@ bool BlockTreeDB::ReadBlockFileInfo(int nFile, CBlockFileInfo& info)
     return Read(std::make_pair(DB_BLOCK_FILES, nFile), info);
 }
 
-bool BlockTreeDB::WriteReindexing(bool fReindexing)
-{
-    if (fReindexing) {
-        return Write(DB_REINDEX_FLAG, uint8_t{'1'});
-    } else {
-        return Erase(DB_REINDEX_FLAG);
-    }
-}
-
 void BlockTreeDB::ReadReindexing(bool& fReindexing)
 {
     fReindexing = Exists(DB_REINDEX_FLAG);
@@ -73,24 +64,6 @@ void BlockTreeDB::ReadReindexing(bool& fReindexing)
 bool BlockTreeDB::ReadLastBlockFile(int& nFile)
 {
     return Read(DB_LAST_BLOCK, nFile);
-}
-
-bool BlockTreeDB::WriteBatchSync(const std::vector<std::pair<int, const CBlockFileInfo*>>& fileInfo, int nLastFile, const std::vector<const CBlockIndex*>& blockinfo)
-{
-    CDBBatch batch(*this);
-    for (const auto& [file, info] : fileInfo) {
-        batch.Write(std::make_pair(DB_BLOCK_FILES, file), *info);
-    }
-    batch.Write(DB_LAST_BLOCK, nLastFile);
-    for (const CBlockIndex* bi : blockinfo) {
-        batch.Write(std::make_pair(DB_BLOCK_INDEX, bi->GetBlockHash()), CDiskBlockIndex{bi});
-    }
-    return WriteBatch(batch, true);
-}
-
-bool BlockTreeDB::WriteFlag(const std::string& name, bool fValue)
-{
-    return Write(std::make_pair(DB_FLAG, name), fValue ? uint8_t{'1'} : uint8_t{'0'});
 }
 
 bool BlockTreeDB::ReadFlag(const std::string& name, bool& fValue)

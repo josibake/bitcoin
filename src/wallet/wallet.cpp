cbf4eb4cebf04ba79b735662b7758561d8d0ea92
Novo <eunovo9@gmail.com>
2025-06-25 13:26:56 +0100

    wallet/rpc: support sp address in getaddressinfo

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1599,6 +1599,15 @@ isminetype CWallet::IsMine(const CTxOut& txout) const
 isminetype CWallet::IsMine(const CTxDestination& dest) const
 {
     AssertLockHeld(cs_wallet);
+    if (IsWalletFlagSet(WALLET_FLAG_SILENT_PAYMENTS)) {
+        // Check if the destination is a silent payment address
+        for (SilentPaymentDescriptorScriptPubKeyMan* sp_spkm : GetSilentPaymentsSPKMs()) {
+            auto ismine{sp_spkm->IsMine(dest)};
+            if (ismine) {
+                return ismine;
+            }
+        }
+    }
     return IsMine(GetScriptForDestination(dest));
 }
 
@@ -3470,6 +3479,20 @@ std::set<ScriptPubKeyMan*> CWallet::GetScriptPubKeyMans(const CScript& script) c
     Assume(std::all_of(spk_mans.begin(), spk_mans.end(), [&script, &sigdata](ScriptPubKeyMan* spkm) { return spkm->CanProvide(script, sigdata); }));
 
     return spk_mans;
+}
+
+std::set<ScriptPubKeyMan*> CWallet::GetScriptPubKeyMans(const CTxDestination& dest) const
+{
+    CScript scriptPubKey{GetScriptForDestination(dest)};
+    if (scriptPubKey.empty() && IsWalletFlagSet(WALLET_FLAG_SILENT_PAYMENTS)) {
+        for (SilentPaymentDescriptorScriptPubKeyMan* sp_spkm : GetSilentPaymentsSPKMs()) {
+            if (sp_spkm->IsMine(dest)) {
+                return {sp_spkm};
+            }
+        }
+    }
+
+    return GetScriptPubKeyMans(scriptPubKey);
 }
 
 ScriptPubKeyMan* CWallet::GetScriptPubKeyMan(const uint256& id) const
